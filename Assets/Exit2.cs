94510130d6bc03467951f5141543328b6251d38a
CodeNinjasEtobicoke <judeolmo@gmail.com>
2024-10-17 17:47:37 -0400

    y
    y

---
+++ Assets/Exit2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject gemGreen;
+    public GameObject Level1A_BG;
+    public string teleporterDestination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gemGreen.activeInHierarchy == false)
+        {
+            Level1A_BG.GetComponent<GameManager>().TeleportOpen(teleporterDestination);
+        }
+
+    }
+}
